2a49368e5de41dd261de04dee13294be91bbd7b7
Cynthia E <cynthia8499@hotmail.com>
2020-06-23 11:21:34 -0400

    calc soln

--- questions/49_calc/calc.js
+++ questions/49_calc/calc.js
@@ -1,7 +1,25 @@
 // - Write a chainable Calculator that can add, subtract,and multiply
 
 class chainCalc {
-  //write code here
+  constructor(num){
+    this.num = num
+    if (typeof num !== 'number'){
+      throw Error
+    }
+    this.result = this.num
+  }
+  add(numToAdd){
+    this.result += numToAdd
+    return this
+  }
+  sub(numToSub){
+    this.result -= numToSub
+    return this
+  }
+  mul(numToMult){
+    this.result *= numToMult
+    return this
+  }
 }
 
 module.exports = { chainCalc };
