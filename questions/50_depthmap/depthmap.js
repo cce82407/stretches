4575270b5bf9eadd8e5957f05f46db66b1b73c62
Cynthia E <cynthia8499@hotmail.com>
2020-06-24 18:55:04 -0400

    depthmap fail

--- questions/50_depthmap/depthmap.js
+++ questions/50_depthmap/depthmap.js
@@ -2,7 +2,21 @@
 //Write a function to recursively "dig" through this object installing a depth key along the way
 //return the object with it's depth indicated
 
-const recordDepth = () => {
+const recordDepth = (obj) => {
+  if (typeof obj !== 'object'){
+    throw Error
+  }
+  if (obj.depth){
+    return obj
+  }
+  let level = 0
+  for (let key in obj){
+    if (typeof obj[key] === 'object'){
+      obj.depth = level
+      level++
+      recordDepth(obj[key])
+    }
+  }
   //write code here
 };
 
