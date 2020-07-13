b0f2ab94a43ec675114818132e23ce91c07ce2c5
Cynthia E <cynthia8499@hotmail.com>
2020-07-13 18:56:12 -0400

    fail

--- questions/53_balancedArr/balancedArr.js
+++ questions/53_balancedArr/balancedArr.js
@@ -1,5 +1,11 @@
-const balanced = () => {
+const balanced = (arr) => {
   //YOUR CODE HERE
+
+  //my idea: loop through the array and use reducer to find sums of the different pieces of the array
+  // for (i = 0; i < Math.floor(arr.length / 2); i++) {
+  return arr.reduce((acc, el) => {
+    return acc + el;
+  }, 0);
 };
 
 module.exports = balanced;
