63937ac2c312c2b92b792894e3cf5c47ac60a7c1
Cynthia E <cynthia8499@hotmail.com>
2020-07-18 09:21:18 -0400

    almost!

--- questions/54_fold/fold.js
+++ questions/54_fold/fold.js
@@ -1,5 +1,21 @@
-const fold = () => {
-  //YOUR CODE HERE
+const fold = (arr, int) => {
+  let finalArr = [];
+
+  function helperFold(arr) {
+    if (arr.length === 0) {
+      return finalArr;
+    }
+    if (arr.length === 1) {
+      finalArr.push(arr[0]);
+    }
+    let sum = arr[0] + arr[arr.length - 1];
+    console.log(sum);
+    finalArr.push(sum);
+    console.log(finalArr);
+    truncatedArr = arr.slice(1, arr.length - 1);
+    return helperFold(truncatedArr);
+  }
+  return helperFold(arr);
 };
 
 module.exports = fold;
